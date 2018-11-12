29b3a897380b78d38c95df59c06f6bac53fdb202
gingerninja1 <qjohnson1@gmail.com>
2018-11-12 19:45:40 +0000

    Update search with category name

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,7 +19,7 @@ class ProfilesController < Devise::RegistrationsController
       @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id
   INNER JOIN user_categories ON user_categories.user_id = users.id
   INNER JOIN categories ON categories.id = user_categories.category_id
-").where("first_name LIKE ? OR last_name LIKE ? OR bio LIKE ? OR skills LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order("created_at DESC").uniq
+").where("first_name LIKE ? OR last_name LIKE ? OR bio LIKE ? OR skills LIKE ? OR categories.name LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order("created_at DESC").uniq
     else
       @profiles = Profile.all.order("created_at DESC")
     end
