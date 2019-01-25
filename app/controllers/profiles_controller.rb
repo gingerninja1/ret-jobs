57247ac2e11c579dfd13ba80f7767ef5337bdec4
gingerninja1 <qjohnson1@gmail.com>
2019-01-25 16:59:38 +0000

    update profile page roles section

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,14 +15,15 @@ class ProfilesController < Devise::RegistrationsController
   end
   
   def search
-    @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("role_id = 2")
+    user_ip = request.remote_ip
+    @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("role_id = 2 AND (zip = #{user_ip})")
     if params[:search]
       @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id
   INNER JOIN user_categories ON user_categories.user_id = users.id
   INNER JOIN categories ON categories.id = user_categories.category_id
   INNER JOIN user_roles ON user_roles.user_id = users.id
   INNER JOIN roles ON roles.id = user_roles.role_id
-").where("first_name LIKE ? OR last_name LIKE ? OR bio LIKE ? OR skills LIKE ? OR categories.name LIKE ? AND roles.id = 2", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order("created_at DESC").uniq
+").where("first_name LIKE ? OR last_name LIKE ? OR bio LIKE ? OR skills LIKE ? OR zip LIKE ? OR categories.name LIKE ? AND roles.id = 2", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order("created_at DESC").uniq
     else
       @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("role_id = 2").order("created_at DESC")
     end
