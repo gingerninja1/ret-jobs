3da24a2c800c4b1f593cf37f4f1f6be509b2afbe
gingerninja1 <qjohnson1@gmail.com>
2019-02-11 20:07:58 +0000

    Zip specificity added to search

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,17 +15,22 @@ class ProfilesController < Devise::RegistrationsController
   end
   
   def search
-    user_ip = request.remote_ip
-    @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("role_id = 2 AND (zip = #{user_ip})")
+    if current_user
+      user_zip = current_user.profile.zip
+    else
+      user_zip = Geocoder.search(request.remote_ip).first.postal
+    end
+
+    #@profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("roles.id = 2 AND zip = #{user_zip}")
     if params[:search]
       @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id
   INNER JOIN user_categories ON user_categories.user_id = users.id
   INNER JOIN categories ON categories.id = user_categories.category_id
   INNER JOIN user_roles ON user_roles.user_id = users.id
   INNER JOIN roles ON roles.id = user_roles.role_id
-").where("first_name LIKE ? OR last_name LIKE ? OR bio LIKE ? OR skills LIKE ? OR zip LIKE ? OR categories.name LIKE ? AND roles.id = 2", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order("created_at DESC").uniq
+").where("(first_name LIKE ? OR last_name LIKE ? OR skills LIKE ? OR categories.name LIKE ?) AND roles.id = 2 AND profiles.zip = ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", user_zip).order("last_name ASC").uniq
     else
-      @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("role_id = 2").order("created_at DESC")
+      @profiles = Profile.joins("INNER JOIN users ON users.id = profiles.user_id").where("role_id = 2 AND zip = ?", user_zip).order("last_name ASC")
     end
     
     render 'results'
