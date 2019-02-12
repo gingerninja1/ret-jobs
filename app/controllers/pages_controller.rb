5f463f624e0981b0924006e45c283db8477562c4
gingerninja1 <qjohnson1@gmail.com>
2019-02-12 14:28:57 +0000

    Update local ratings on homepage to specific zip of user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   
   def index
+    if current_user
+      user_zip = current_user.profile.zip
+    else
+      user_zip = Geocoder.search(request.remote_ip).first.postal
+    end
+    
     @local_users = Profile.where(zip: Geocoder.search(request.remote_ip).first.postal)
-    @best_reviews = Review.where(rating: 5).order(created_at: :desc)
+    @best_reviews = Review.joins(:profile).where("profiles.zip = ? AND rating = ?", user_zip, 5).order(created_at: :desc)
   end
   
   def about
